2e96f4663d0bbdb2150726f49c95daf6ce4fc604
accrane <austincrane@Austins-MBP-2.attlocal.net>
2020-06-11 14:00:05 -0400

    loose fance box

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -6,56 +6,56 @@
  *	Developed by: Lisa DeBona
  */
 jQuery(document).ready(function ($) {
-  var swiper = new Swiper('#slideshow', {
-    effect: 'fade',
+  // var swiper = new Swiper('#slideshow', {
+  //   effect: 'fade',
 
-    /* "fade", "cube", "coverflow" or "flip" */
-    loop: true,
-    noSwiping: false,
-    simulateTouch: false,
-    speed: 1000,
-    autoplay: {
-      delay: 4000
-    }
-  });
-  $('[data-fancybox]').fancybox({
-    protect: true,
-    autoWidth: true
-  });
-  /* Smooth Scroll */
+  //   /* "fade", "cube", "coverflow" or "flip" */
+  //   loop: true,
+  //   noSwiping: false,
+  //   simulateTouch: false,
+  //   speed: 1000,
+  //   autoplay: {
+  //     delay: 4000
+  //   }
+  // });
+  // $('[data-fancybox]').fancybox({
+  //   protect: true,
+  //   autoWidth: true
+  // });
+  // /* Smooth Scroll */
 
-  $('a[href*="#"]').not('[href="#"]').not('[href="#0"]').click(function (event) {
-    // On-page links
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-      // Figure out element to scroll to
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']'); // Does a scroll target exist?
+  // $('a[href*="#"]').not('[href="#"]').not('[href="#0"]').click(function (event) {
+  //   // On-page links
+  //   if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+  //     // Figure out element to scroll to
+  //     var target = $(this.hash);
+  //     target = target.length ? target : $('[name=' + this.hash.slice(1) + ']'); // Does a scroll target exist?
 
-      if (target.length) {
-        // Only prevent default if animation is actually gonna happen
-        event.preventDefault();
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000, function () {
-          // Callback after animation
-          // Must change focus!
-          var $target = $(target);
-          $target.focus();
+  //     if (target.length) {
+  //       // Only prevent default if animation is actually gonna happen
+  //       event.preventDefault();
+  //       $('html, body').animate({
+  //         scrollTop: target.offset().top
+  //       }, 1000, function () {
+  //         // Callback after animation
+  //         // Must change focus!
+  //         var $target = $(target);
+  //         $target.focus();
 
-          if ($target.is(":focus")) {
-            // Checking if the target was focused
-            return false;
-          } else {
-            $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+  //         if ($target.is(":focus")) {
+  //           // Checking if the target was focused
+  //           return false;
+  //         } else {
+  //           $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
 
-            $target.focus(); // Set focus again
-          }
+  //           $target.focus(); // Set focus again
+  //         }
 
-          ;
-        });
-      }
-    }
-  });
+  //         ;
+  //       });
+  //     }
+  //   }
+  // });
   /*
   *
   *	Wow Animation
